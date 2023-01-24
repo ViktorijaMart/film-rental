e0fab93949724bc552add6f8d07581f451c0f1d9
LhunaCat <viktorijamartinaityte@gmail.com>
2023-01-24 15:56:56 +0200

    src architecture, DI, Router, Database

---
+++ src/Framework/DbConnection.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Vikto\CarProject\Framework;
+
+class DbConnection
+{
+    private static $instance = null;
+    private $conn;
+
+    private $host = 'localhost:3306';
+    private $user = 'root';
+    private $password = '';
+    private $name = 'film_rental';
+
+    private function __construct()
+    {
+        $this->conn = new \PDO(
+            "mysql:host=$this->host;dbname=$this->name",
+            $this->user,
+            $this->password,
+            [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION],
+        );
+    }
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new DbConnection();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+}
---
+++ src/Framework/Router.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Vikto\FilmRentalProject\Framework;
+
+class Router
+{
+    public function process (): void
+    {
+        echo 'Hello from Router';
+    }
+}
