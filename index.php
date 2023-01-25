39ccd99f5abd328fa4ab1685bcb3bd44f94953a3
LhunaCat <viktorijamartinaityte@gmail.com>
2023-01-25 16:05:39 +0200

    fixed namespace mistake

--- index.php
+++ index.php
@@ -6,5 +6,12 @@ require_once './vendor/autoload.php';
 $container = new \Vikto\FilmRentalProject\Framework\DIContainer();
 
 $request = str_replace('/FilmRentalProject', '', $_SERVER['REQUEST_URI']);
-$router = $container->get('Vikto\FilmRentalProject\Framework\Router');
-$router->process($request);
+
+try {
+    $router = $container->get('Vikto\FilmRentalProject\Framework\Router');
+    $router->process($request);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+
--- src/Framework/DbConnection.php
+++ src/Framework/DbConnection.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Vikto\CarProject\Framework;
+namespace Vikto\FilmRentalProject\Framework;
 
 class DbConnection
 {
